68a0a0ca71d763119eb6d2c73ea084dec4be8f07
jacobsanosh <jacobsanosh@gmail.com>
2021-03-05 10:07:31 +0530

    Login advanced

--- app/src/main/java/org/gptccherthala/virtualqueue/LoginActivity.java
+++ app/src/main/java/org/gptccherthala/virtualqueue/LoginActivity.java
@@ -15,8 +15,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import org.gptccherthala.virtualqueue.business.BusinessHomeActivity;
 import org.gptccherthala.virtualqueue.user.UserHomeActivity;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText etPassword;
     Button loginBtn;
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +44,35 @@ public class LoginActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         if (currentUser != null) {
-            Intent UserHomeActivity = new Intent(getApplicationContext(), org.gptccherthala.virtualqueue.user.UserHomeActivity.class);
-            startActivity(UserHomeActivity);
-            LoginActivity.this.finish();
+            mDatabase = FirebaseDatabase.getInstance().getReference("/users/");
+            String Uid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+            mDatabase.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    boolean flag = false;
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        System.out.println("Uid/; " + dataSnapshot.getKey());
+                        System.out.println("Uid curr; " + Uid);
+                        if(dataSnapshot.getKey().equals(Uid)){
+                            flag = true;
+                        }
+                    }
+                    if(flag){
+                        Intent UserHomeActivity = new Intent(getApplicationContext(), UserHomeActivity.class);
+                        startActivity(UserHomeActivity);
+                        LoginActivity.this.finish();
+                    } else {
+                        Intent BusinessHomeActivity = new Intent(getApplicationContext(), BusinessHomeActivity.class);
+                        startActivity(BusinessHomeActivity);
+                        LoginActivity.this.finish();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
         }
 
         etEmail = findViewById(R.id.email);
@@ -54,9 +89,38 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Intent UserHomeActivity = new Intent(getApplicationContext(), UserHomeActivity.class);
-                            startActivity(UserHomeActivity);
-                            LoginActivity.this.finish();
+                            mDatabase = FirebaseDatabase.getInstance().getReference("/users/");
+                            String Uid = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+                            // for getting the current user id
+                            mDatabase.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    if (snapshot.exists()) {
+                                        boolean flag = false;
+                                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                            System.out.println("Uid/; " + dataSnapshot.getKey());
+                                            System.out.println("Uid curr; " + Uid);
+                                            if(dataSnapshot.getKey().equals(Uid)){
+                                                flag = true;
+                                            }
+                                        }
+                                        if(flag){
+                                            Intent UserHomeActivity = new Intent(getApplicationContext(), UserHomeActivity.class);
+                                            startActivity(UserHomeActivity);
+                                            LoginActivity.this.finish();
+                                        } else {
+                                            Intent BusinessHomeActivity = new Intent(getApplicationContext(), BusinessHomeActivity.class);
+                                            startActivity(BusinessHomeActivity);
+                                            LoginActivity.this.finish();
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
                         } else {
                             Toast.makeText(LoginActivity.this, "Incorrect credentials", Toast.LENGTH_LONG).show();
                         }
