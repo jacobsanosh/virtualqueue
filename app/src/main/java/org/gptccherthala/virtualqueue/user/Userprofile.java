f7a434f7d5fab69fa6b8729d1ca0a6d9b16aa4e0
jacobsanosh <jacobsanosh@gmail.com>
2021-03-06 10:41:51 +0530

    profilepage

---
+++ app/src/main/java/org/gptccherthala/virtualqueue/user/Userprofile.java
@@ -0,0 +1,16 @@
+package org.gptccherthala.virtualqueue.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.gptccherthala.virtualqueue.R;
+
+public class Userprofile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userprofile);
+    }
+}
